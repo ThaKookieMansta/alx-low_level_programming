d0323625ec3ba6158fdb643252052209f3ea8527
Chris Mukirae <chriskagiko@gmail.com>
2023-02-16 14:14:37 +0300

    mandatory tasks corrections.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
         char str[55];
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
         char x;
@@ -13,6 +20,6 @@ int main(void)
         printf("Size of a long int: %d byte(s)\n", sizeof(z));
         printf("Size of a long long int: %d byte(s)\n", sizeof(a));
         printf("Size of a float: %d byte(s)\n", sizeof(b));
-
         return (0);
 }
+
