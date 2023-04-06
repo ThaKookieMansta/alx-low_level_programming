0fd8fb510191d9c8e428b806d8e80325bb47eb1e
Chris Mukirae <chriskagiko@gmail.com>
2023-04-06 17:38:33 +0300

    Betty corrections
    This commit has corrected the betty errors

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,7 +3,7 @@
 /**
  * set_bit - A function that sets the value of a bit
  * to 1 at a given index.
- * @n: The number 
+ * @n: The number
  * @index: The index to set the bit
  * Return: 1 if it worked, or -1 if error occured
  */
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,7 +3,7 @@
 /**
  * clear_bit - A function that sets the value of a bit to 0
  * at a given index.
- * @n: The number 
+ * @n: The number
  * @index: The index where to set bit starting from 0
  * Return: 1 if it worked, or -1 if an error occurred
  */
