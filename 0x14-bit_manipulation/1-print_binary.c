c1afa7a92de6dda4fe72e336baa1c872f3db5023
Chris Mukirae <chriskagiko@gmail.com>
2023-04-06 18:06:25 +0300

    Corection on 1-print_binary.c
    We are now using recursion to get the solution

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,24 +9,10 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int maxBinary = 32768, point = 0;
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (maxBinary)
-	{
-		if (point == 1 && (n & maxBinary) == 0)
-		{
-			_putchar('0');
-		}
-		else if ((n & maxBinary) != 0)
-		{
-			_putchar('1');
-			point = 1;
-		}
-		maxBinary >>= 1;
-	}
+	_putchar((n & 1) + '0');
 }
