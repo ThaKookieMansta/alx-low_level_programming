0764ad2455e7d44a77a5acc59010084fa2c36b65
Chris Mukirae <chriskagiko@gmail.com>
2023-04-08 13:54:01 +0300

    0-read_textfile.c betty corrections
    Correct the number of characters on line 31

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fileRead = read(fileOpen, fileBuffer, letters);
 	fileWrite = write(STDOUT_FILENO, fileBuffer, fileRead);
 
-	if (fileOpen == -1 || fileRead == -1 || fileWrite == -1 || fileWrite != fileRead)
+	if (fileOpen == -1 || fileRead == -1 || fileWrite == -1 || 
+	fileWrite != fileRead)
 	{
 		free(fileBuffer);
 		return (0);
