6dbaa24b5605b1e613d80de6cd86640b8bf9c592
Chris Mukirae <chriskagiko@gmail.com>
2023-06-19 20:02:06 +0300

    Task 3 correction.1
    This commit is for marking of task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ThaKookieMansta/alx-low_level_programming/blob/main/0x18-dynamic_libraries/guarenteedwin.so
+wget -P /tmp https://shorturl.at/brtEU
 export LD_PRELOAD=/tmp/guarenteedwin.so
