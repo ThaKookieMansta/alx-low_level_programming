eb90ab61683f517d798723ffe293667baf6cc6d0
Chris Mukirae <chriskagiko@gmail.com>
2023-07-07 11:52:10 +0300

    Corrections 0 v.1
    Corrections on the file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,20 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int li = 0;
-	hash_table_t *new_table = NULL;
+	hash_table_t *new_table;
+	unsigned long int li;
 
 	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!new_table->array)
+	new_table->array = malloc(size * sizeof(hash_node_t *));
+	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	for (; li < size; li++)
-		(new_table->array)[li] = NULL;
+	for (li = 0; li < size; li++)
+		new_table->array[li] = NULL;
 	return (new_table);
 }
